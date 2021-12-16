91022030e54765ab171a8fabccee005b201d04c9
navee <naveenarameshvardhini@gmail.com>
2021-12-16 11:05:12 +0530

    feat:Userdetails

---
+++ foodo/lib/Auth/userdetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Userdetails extends StatefulWidget {
+  const Userdetails({Key? key}) : super(key: key);
+
+  @override
+  _UserdetailsState createState() => _UserdetailsState();
+}
+
+class _UserdetailsState extends State<Userdetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
