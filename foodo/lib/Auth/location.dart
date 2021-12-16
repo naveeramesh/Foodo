fefeeb4cb13928cad8d509ef3371e3c007a0601f
navee <naveenarameshvardhini@gmail.com>
2021-12-16 14:56:33 +0530

    feat:Userlocation Ui

--- foodo/lib/Auth/location.dart
+++ foodo/lib/Auth/location.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodo/constants/text.dart';
+import 'package:lottie/lottie.dart';
+import 'package:lottie/lottie.dart';
 
 class Location extends StatefulWidget {
   const Location({Key? key}) : super(key: key);
@@ -9,7 +12,24 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
   @override
+  void initState() {
+    
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+            child: Container(
+          height: MediaQuery.of(context).size.height / 3,
+          child: Lottie.network(
+              "https://assets1.lottiefiles.com/temp/lf20_EVDaJ0.json"),
+        )),
+        Helper.text("Fetching Location", 20, 0, Colors.black, FontWeight.bold)
+      ],
+    ));
   }
 }
