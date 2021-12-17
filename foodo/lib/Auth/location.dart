12a0fafb62e2e09592a8878e9ce3611ecb06e08f
navee <naveenarameshvardhini@gmail.com>
2021-12-17 09:51:53 +0530

    feat:Fetched User location

--- foodo/lib/Auth/location.dart
+++ foodo/lib/Auth/location.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:foodo/constants/text.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:lottie/lottie.dart';
+
 class Location extends StatefulWidget {
   const Location({Key? key}) : super(key: key);
 
@@ -9,11 +14,16 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  late StreamSubscription<Position> streamSubscription;
+  var lattiude = '';
+  var longitude = '';
+  var address = '';
   @override
   void initState() {
-    
+    getlocation();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,5 +39,50 @@ class _LocationState extends State<Location> {
         Helper.text("Fetching Location", 20, 0, Colors.black, FontWeight.bold)
       ],
     ));
+  }
+
+  void getlocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    streamSubscription =
+        Geolocator.getPositionStream().listen((Position position) {
+      lattiude = "${position.latitude}";
+      longitude = "${position.longitude}";
+      print(lattiude);
+      print(longitude);
+      getAddress(position);
+    });
+  }
+
+  Future<void> getAddress(Position position) async {
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.latitude);
+    Placemark location = placemark[0];
+    address =
+        "${location.name}" +" "+ "${location.street}" + " "+"${location.country}";
+    print(address);
   }
 }
