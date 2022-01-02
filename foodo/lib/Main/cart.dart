79d4ea6a4670430d50042efec85a1aece67665d6
navee <naveenarameshvardhini@gmail.com>
2022-01-02 17:27:54 +0530

    feat:cart/Ui

--- foodo/lib/Main/cart.dart
+++ foodo/lib/Main/cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class Cart extends StatefulWidget {
-  const Cart({ Key? key }) : super(key: key);
+  const Cart({Key? key}) : super(key: key);
 
   @override
   _CartState createState() => _CartState();
@@ -9,8 +10,6 @@ class Cart extends StatefulWidget {
 class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
